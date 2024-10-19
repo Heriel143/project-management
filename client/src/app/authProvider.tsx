81d6f6db7b6bc6196b0efd419b8fbf0d05451756
Heriel David <herieldavid143@gmail.com>
2024-10-19 08:37:21 +0300

    changed project title

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -44,7 +44,7 @@ const formFields = {
 
 const AuthProvider = ({ children }: any) => {
   return (
-    <div>
+    <div className="flex h-screen flex-col items-center justify-center">
       <Authenticator formFields={formFields}>
         {({ user }: any) =>
           user ? (
